69a049a6583406ba91f6aa6dc37c871893ac5613
David Janes <david@consensas.com>
2021-04-20 13:08:45 -0400

    add and add.all

---
+++ test/add.js
@@ -0,0 +1,69 @@
+/*
+ *  test/add.js
+ *
+ *  David Janes
+ *  IOTDB.org
+ *  20201-04-20
+ *
+ *  Copyright (2013-2021) David P. Janes
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict";
+
+const _ = require("iotdb-helpers")
+const zip = require("..")
+const fs = require("iotdb-fs")
+
+const assert = require("assert");
+const path = require("path");
+
+describe("all", function() {
+    const FILENAMES = [
+        "contents/a.json",
+        "contents/icon.png",
+        "contents/unicode.txt"
+    ]
+
+    it("add works", function(done) {
+        _.promise()
+            .then(zip.initialize)
+            .add("zip$root", path.join(__dirname, "data"))
+            .add("path", FILENAMES[0])
+            .then(zip.add)
+            .make(sd => {
+                const got = _.keys(sd.zip.files)
+                const want = [ FILENAMES[0], ]
+
+                assert.deepEqual(got, want)
+            })
+
+            .end(done, {})
+    })
+    it("add.all works", function(done) {
+        _.promise()
+            .then(zip.initialize)
+            .add("zip$root", path.join(__dirname, "data"))
+            .add("paths", FILENAMES)
+            .then(zip.add.all)
+            .make(sd => {
+                const got = _.keys(sd.zip.files)
+                const want = FILENAMES
+
+                assert.deepEqual(got, want)
+            })
+
+            .end(done, {})
+    })
+})
